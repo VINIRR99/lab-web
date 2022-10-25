66ff9e2fc87a91ba2ea43a6f0a1f1df7a05e600a
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-25 10:34:13 -0400

    store events

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { User } from "./models/User";
 
 const jacob = new User({ name: 'Jacob Tory' });
-console.log(jacob.get('name'));
-console.log(jacob.get('age'));
+// console.log(jacob.get('name'));
+// console.log(jacob.get('age'));
 
 jacob.set({ name: 'Jacob Tate' });
 jacob.set({ age: 20 });
 
-console.log(jacob.get('name'));
-console.log(jacob.get('age'));
+// console.log(jacob.get('name'));
+// console.log(jacob.get('age'));
+
+jacob.on('change', () => {});
+jacob.on('change', () => {});
+jacob.on('click', () => {});
+console.log(jacob.events)
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,10 +3,20 @@ interface UserProps {
     age?: number;
 };
 
+type Callback = () => void;
+
 export class User {
+    events: { [key: string]: Callback[] } = {};
+
     constructor(private data: UserProps) {};
 
     get = (propName: string): (string | number) => this.data[propName];
 
     set = (update: UserProps): void => {Object.assign(this.data, update)};
+
+    on = (eventName: string, callback: Callback): void => {
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    };
 };
