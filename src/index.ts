635529a5b459d64c095852d1aaa6a5b1d7c9c4e9
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-25 11:54:08 -0400

    use axios

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-import { User } from "./models/User";
+import axios from "axios";
 
-const jacob = new User({ name: 'Jacob Tory' });
-// console.log(jacob.get('name'));
-// console.log(jacob.get('age'));
-
-jacob.set({ name: 'Jacob Tate' });
-jacob.set({ age: 20 });
-
-// console.log(jacob.get('name'));
-// console.log(jacob.get('age'));
-
-jacob.on('change', () => {console.log('Change #1')});
-jacob.on('change', () => {console.log('Change #2')});
-jacob.on('click', () => {console.log('Click was triggered')});
-
-jacob.trigger('change');
-jacob.trigger('click');
-jacob.trigger('save');
+// axios.post('http://localhost:3000/users', { name: 'Jacob Tate', age: 20 });
+axios.get('http://localhost:3000/users/1').then(user => console.log(user.data));
