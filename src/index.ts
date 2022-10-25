75446cdb215f2a659966182575e0fe6a365397f9
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-25 15:07:28 -0400

    Eventing

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { User } from "./models/User";
 
 // axios.post('http://localhost:3000/users', { name: 'Jacob Tate', age: 20 });
-// axios.delete('http://localhost:3000/users/2');
+// axios.delete('http://localhost:3000/users/4');
 
-const user = new User({ name: 'Mario Battali', age: 50 });
-user.save();
+// const user = new User({ name: 'Mario Battali', age: 50 });
+// user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Eventing } from "./Eventing";
 import axios, { AxiosResponse } from "axios";
 
 interface UserProps {
@@ -6,30 +7,14 @@ interface UserProps {
     age?: number;
 };
 
-type Callback = () => void;
-
 export class User {
-    events: { [key: string]: Callback[] } = {};
+    events: Eventing = new Eventing();
 
     constructor(private data: UserProps) {};
 
     get = (propName: string): (string | number) => this.data[propName];
 
     set = (update: UserProps): void => {Object.assign(this.data, update)};
-
-    on = (eventName: string, callback: Callback): void => {
-        const handlers = this.events[eventName] || [];
-        handlers.push(callback);
-        this.events[eventName] = handlers;
-    };
-
-    trigger = (eventName: string): void => {
-        const handlers = this.events[eventName];
-
-        if (!handlers || (handlers.length === 0)) return;
-
-        handlers.forEach((callback: Callback): void => {callback()});
-    };
 
     fetch = (): void => {
         axios.get(`http://localhost:3000/users/${this.get('id')}`).then((response: AxiosResponse): void => {
