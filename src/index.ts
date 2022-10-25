d283347e1cd8b65ec799a15ed98d5a0091b20ea3
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-25 10:48:36 -0400

    User.trigger

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,10 @@ jacob.set({ age: 20 });
 // console.log(jacob.get('name'));
 // console.log(jacob.get('age'));
 
-jacob.on('change', () => {});
-jacob.on('change', () => {});
-jacob.on('click', () => {});
-console.log(jacob.events)
+jacob.on('change', () => {console.log('Change #1')});
+jacob.on('change', () => {console.log('Change #2')});
+jacob.on('click', () => {console.log('Click was triggered')});
+
+jacob.trigger('change');
+jacob.trigger('click');
+jacob.trigger('save');
