cb3d8634fafb417701dfca8961346fa0c0178215
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-26 17:47:30 -0400

    UserCollection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { User } from "./models/User";
+import { UserCollection } from "./models/UserCollection";
 
-const user = User.buildUser({ id: 7 });
+// const user = User.buildUser({ id: 9 });
 
-user.fetch();
+// user.on('change', (): void => {console.log(user)});
 
-user.on('change', (): void => {console.log(user)});
+// user.fetch();
 
-user.save();
+const users = new UserCollection('http://localhost:3000/users');
+
+users.on('change', (): void => {console.log(users)});
+
+users.fetch();
