3a46b02d6fad44fa5595b4810fb24bfc898fbd10
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-26 12:08:27 -0400

    User - on(), trigger(), get()

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { User } from "./models/User";
 
 const user = new User({ name: 'Luigi Mario', age: 45 });
-const name = user.attributes.get('name');
+
+const name = user.get('name');
 console.log(name);
-const age = user.attributes.get('age');
-console.log(age)
+
+const age = user.get('age');
+console.log(age);
+
+user.on('change', () => {console.log('User was changed!')});
+
+user.trigger('change');
