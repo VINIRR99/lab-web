a5820e3909d98dc49f36298dd6104f5ac0fb5a21
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-27 10:59:58 -0400

    Binding Event Handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -5,12 +5,37 @@ export class UserForm {
         <div>
             <h1>User Form</h1>
             <input />
+            <button>Click Me</button>
         </div>
     `;
+
+    onButtonClick = (): void => {console.log('Hi There')};
+
+    onHeaderHover = (): void => {console.log('H1 was hovered!')};
+
+    eventsMap = (): { [key: string]: () => void } => {return {
+        'click:button': this.onButtonClick,
+        'mouseenter:h1': this.onHeaderHover
+    }};
+
+    bindEvents = (fragment: DocumentFragment): void => {
+        const eventsMap = this.eventsMap();
+
+        for (let eventKey in eventsMap) {
+            const [eventName, selector] = eventKey.split(':');
+
+            fragment.querySelectorAll(selector).forEach(element => {
+                element.addEventListener(eventName, eventsMap[eventKey]);
+            });
+        };
+    };
 
     render = (): void => {
         const template = document.createElement('template');
         template.innerHTML = this.template();
+
+        this.bindEvents(template.content);
+
         this.parent.append(template.content);
     };
 };
